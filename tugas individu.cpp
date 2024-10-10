d163b05dc3f7cd147f5c05cb98b8e6c37bb67874
nessanovianti <nessanovianti05@gmail.com>
2024-10-10 10:25:32 +0700

    Add files via upload

---
+++ tugas individu.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n; "Masukkan jumlah data: ";
+	cout << "Masukkan jumlah data: ";
+	cin >> n;
+
+	int jumlahGanjil = 0;
+	int jumlahGenap = 0;
+
+	for (int i = 1; i < n; ++i) {
+
+		if (i % 2 == 0) {
+			jumlahGanjil += i;
+		}
+		else {
+			jumlahGenap += i;
+		}
+	}
+	cout << "Jumlah total bilangan genap: " << jumlahGanjil << endl;
+	cout << "Jumlah total bilangan ganjil: " << jumlahGenap << endl;
+}
